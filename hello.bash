594a48041f2800baa6513bbfe53865e1e4d47b8d
Sahil Suri <34906648+sahilsuri008@users.noreply.github.com>
2022-06-05 09:25:18 +0530

    Update hello.bash

--- hello.bash
+++ hello.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Welcome to my Jenkins server"
-echo "You are logged in to $(uname -n)"
+echo "You are logged in to $(uname -a)"
 echo "Do not break anything!"
 echo "Today is $(date)"
 echo "Testing GitHub Jenkins webhook based polling"
